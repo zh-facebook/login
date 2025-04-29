f4b8cb3350ac7dbe8a9986d709a53e40bfe24f92
Joanne <joanne2704792@gmail.com>
2025-04-29 21:51:20 +0800

    first

--- BackPage/introduction.js
+++ BackPage/introduction.js
@@ -2,11 +2,15 @@ function toggleContent() {
     var card = document.getElementById("card");
     var content = document.getElementById("content");
 
+
     if (card.classList.contains("hide")) {
         card.classList.remove("hide");
         content.classList.add("hide");
+        document.title = "預告函";
+   
     } else {
         card.classList.add("hide");
         content.classList.remove("hide");
+        document.title = "什麼是釣魚網站？";
     }
 }

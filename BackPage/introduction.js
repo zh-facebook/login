e6c6e82ec1b5d0e1b43c5402317d6d3b54c0d1b4
Joanne <joanne2704792@gmail.com>
2025-05-01 01:42:11 +0800

    update button

--- BackPage/introduction.js
+++ BackPage/introduction.js
@@ -17,7 +17,15 @@ function toggleContent() {
 
 function showAnswer(num){
     event.stopPropagation();
+
+    // 先隱藏所有答案
+    let allAnswers = document.querySelectorAll('.answer');
+    allAnswers.forEach(function(answer) {
+        answer.innerHTML = ''; // 清空所有答案
+    });
+
     let answer = "";
+    let answerElement = document.getElementById("answer" + num); // 根據按鈕對應的答案區域
     switch(num){
         case 1:
             answer = "1. 個人資料外洩<br>2. 金融損失<br>3. 帳號被盜<br>4. 信用評分受損<br>5. 法律責任<br>6. 網路安全風險<br>";
@@ -32,5 +40,5 @@ function showAnswer(num){
             answer = "1. 使用安全的網路連接<br>2. 不隨意點擊不明連結<br>3. 使用防毒軟體<br>4. 定期更改密碼<br>5. 開啟雙重驗證<br>6. 使用密碼管理器<br>";
         break;
     }
-    document.getElementById("answer").innerHTML = answer;
+    answerElement.innerHTML = answer; // 將答案填入對應的區域
 }
